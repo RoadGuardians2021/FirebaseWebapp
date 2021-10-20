e8924034e9d827c59c4cbb6a7d8c1778ffc63dd6
Joshua Garrelts <n272b747@wichita.edu>
2021-10-19 20:00:35 -0500

    Pushing current state of WebAPP

--- public/main.js
+++ public/main.js
@@ -3,17 +3,53 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+      // Grab the current value of what was written to Firestore.
+      const original = snap.data().original;
+
+      // Access the parameter `{documentId}` with `context.params`
+      functions.logger.log('Uppercasing', context.params.documentId, original);
+      
+      const uppercase = original.toUpperCase();
+      
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to Firestore.
+      // Setting an 'uppercase' field in Firestore document returns a Promise.
+      return snap.ref.set({uppercase}, {merge: true});
+    });
+
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBRAzp00jlFqrMlHaBobfgw_ksLxs-0Vf0",
-  authDomain: "icyroaddetection.firebaseapp.com",
-  projectId: "icyroaddetection",
-  storageBucket: "icyroaddetection.appspot.com",
-  messagingSenderId: "1005688725909",
-  appId: "1:1005688725909:web:9cadcefaf822c67903169c"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBRAzp00jlFqrMlHaBobfgw_ksLxs-0Vf0",
+//   authDomain: "icyroaddetection.firebaseapp.com",
+//   projectId: "icyroaddetection",
+//   storageBucket: "icyroaddetection.appspot.com",
+//   messagingSenderId: "1005688725909",
+//   appId: "1:1005688725909:web:9cadcefaf822c67903169c"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-document.getElementById('date').innerHTML = new Date().toDateString();
+// document.getElementById('date').innerHTML = new Date().toDateString();
