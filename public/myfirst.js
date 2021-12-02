06fad8f77e7ae9cc1ff74c456094cf6c9ac89ba8
Joshua Garrelts <n272b747@wichita.edu>
2021-12-01 19:39:22 -0600

    Commit containing all progress made on the cloud functions and containerized app. The cloud functions work in an emulator, but will require further developement to work when deployed.

--- public/myfirst.js
+++ public/myfirst.js
@@ -1,16 +1,22 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "157.245.113.224",
+  host: "34.71.49.21",
   user: "josh",
-  password: "34182057roadguardiansWSU",
+  password: "josh34182057",
   database: "db"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM Curr_Table", function (err, result, fields) {
+  con.query("SELECT * from Temp_Table", function (err, result, fields) {
     if (err) throw err;
+    
     console.log(result);
+
+    Object.keys(result).forEach(function(key) {
+        var row = result[key];
+        console.log(row.device_id);
+    });
   });
 });
